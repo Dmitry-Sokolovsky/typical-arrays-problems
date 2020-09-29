1c0b1c2ac6ac140b4e53233a939944a1fe7f213c
Dmitry-Sokolovsky <dmitry635494@gmail.com>
2020-09-29 22:27:07 +0300

    float: add decision

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 
 exports.min = function min (array) {
+  if (array != undefined && array.length > 0){
+    return Math.min.apply(Math, array);
+  }
+  else{
   return 0;
+  };
 }
 
 exports.max = function max (array) {
+  if (array != undefined && array.length > 0){
+    return Math.max.apply(Math, array);
+  }
+  else{
   return 0;
+  };
 }
 
 exports.avg = function avg (array) {
+  let avg;
+  let a=0;
+  if (array != undefined && array.length > 0){
+    for (let i=0; i<array.length; i++){
+      a+=array[i];
+    }
+    avg = a/array.length;
+    return avg;
+  }
+  else{
   return 0;
+  };
 }
